1f42cccb3bff31af9a3e4a560c31bf3bd318a1d0
arts-unimelb <arts-webmaster@unimelb.edu.au>
2013-09-11 14:32:27 +1000

    user_login_form style is executed once only

--- custom/partnership/js/partnership.js
+++ custom/partnership/js/partnership.js
@@ -2,8 +2,11 @@
   Drupal.behaviors.partnership = {
     attach:function(context, settings) {
 			// User login form
-   		jQuery('form#user-login input#edit-name').attr('size', '60');
-			jQuery('form#user-login input#edit-name').removeClass('fluid'); 
+			jQuery('form#user-login input#edit-name').once('user_login_form_func', function(){
+				jQuery('form#user-login input#edit-name').attr('size', '60');
+      	jQuery('form#user-login input#edit-name').removeClass('fluid');
+
+			});
 		}
   }
 }(jQuery));
