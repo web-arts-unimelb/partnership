bc06c171d0a3b123c607a71c7ea1d51b39ceaec6
arts-unimelb <arts-webmaster@unimelb.edu.au>
2013-09-19 17:02:40 +1000

    ajax reset button for exposed view filter

--- modules/custom/partnership/js/partnership.js
+++ modules/custom/partnership/js/partnership.js
@@ -7,6 +7,17 @@
       	jQuery('form#user-login input#edit-name').removeClass('fluid');
 
 			});
+
+			// Reset button on view exposed filter
+			// https://drupal.org/node/1264316
+			// form_alter is not working for it, not sure why
+			jQuery('form#views-exposed-form-organisation-page-find-partner input#edit-reset').once('reset_exposed_button_func', function(){
+				jQuery('form#views-exposed-form-organisation-page-find-partner input#edit-reset').prop('type', 'reset');
+	      jQuery('form#views-exposed-form-organisation-page-find-partner input#edit-reset').on('click', function(){
+  	      history.go(0);
+    	  });
+			});
 		}
   }
 }(jQuery));
+
