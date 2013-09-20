b435576c2e6fd85a0e9b1c5a1b7e509745f3f2f1
arts-unimelb <arts-webmaster@unimelb.edu.au>
2013-09-20 14:21:56 +1000

    reset button should be working better

--- modules/custom/partnership/js/partnership.js
+++ modules/custom/partnership/js/partnership.js
@@ -12,11 +12,16 @@
 			// https://drupal.org/node/1264316
 			// form_alter is not working for it, not sure why
 			jQuery('form#views-exposed-form-organisation-page-find-partner input#edit-reset').once('reset_exposed_button_func', function(){
-				jQuery('form#views-exposed-form-organisation-page-find-partner input#edit-reset').prop('type', 'reset');
+				jQuery('form#views-exposed-form-organisation-page-find-partner input#edit-reset').prop('type', 'button');
 	      jQuery('form#views-exposed-form-organisation-page-find-partner input#edit-reset').on('click', function(){
-  	      history.go(0);
+					// Reset all value
+					jQuery('form#views-exposed-form-organisation-page-find-partner input#edit-title').prop('value', '');
+					jQuery("form#views-exposed-form-organisation-page-find-partner option:selected").prop("selected", false);
+
+					jQuery("form#views-exposed-form-organisation-page-find-partner input#edit-submit-organisation").trigger('click');		
     	  });
 			});
+		
 		}
   }
 }(jQuery));
