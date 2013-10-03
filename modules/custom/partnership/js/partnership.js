7d77d442da344e4363e502240837ae1b83a8cce1
arts-unimelb <arts-webmaster@unimelb.edu.au>
2013-10-03 16:08:38 +1000

    avoid javascript global variable for now

--- modules/custom/partnership/js/partnership.js
+++ modules/custom/partnership/js/partnership.js
@@ -11,44 +11,43 @@
 			// Reset button on view exposed filter on find partner page.
 			// https://drupal.org/node/1264316
 			// form_alter is not working for it, not sure why
-			var form_id = 'form#views-exposed-form-organisation-page-find-partner';
-			jQuery(form_id + ' input#edit-reset').once('reset_find_partner_func', function(){
-				jQuery(form_id + ' input#edit-reset').prop('type', 'button');
-	      jQuery(form_id + ' input#edit-reset').on('click', function(){
+			jQuery('form#views-exposed-form-organisation-page-find-partner input#edit-reset').once('reset_find_partner_func', function(){
+				jQuery('form#views-exposed-form-organisation-page-find-partner input#edit-reset').prop('type', 'button');
+	      jQuery('form#views-exposed-form-organisation-page-find-partner input#edit-reset').on('click', function(){
+		
 					// Reset all value
-					jQuery(form_id + ' input#edit-title').prop('value', '');
-					jQuery(form_id + " option:selected").prop("selected", false);
+					jQuery('form#views-exposed-form-organisation-page-find-partner input#edit-title').prop('value', '');
+					jQuery("form#views-exposed-form-organisation-page-find-partner option:selected").prop("selected", false);
 
 					// Trigger click
-					jQuery(form_id + " input#edit-submit-organisation").trigger('click');		
+					jQuery("form#views-exposed-form-organisation-page-find-partner input#edit-submit-organisation").trigger('click');		
     	  });
 			});
 
 
 			// Reset button on view exposed filter on find partner relationship page.
-			var form_id = 'form#views-exposed-form-organisation-find-partner-relationship';
-			jQuery(form_id + ' input#edit-reset').once('reset_find_partner_relation_func', function(){
-        jQuery(form_id + ' input#edit-reset').prop('type', 'button');
-        jQuery(form_id + ' input#edit-reset').on('click', function(){
+			jQuery('form#views-exposed-form-organisation-find-partner-relationship input#edit-reset').once('reset_find_partner_relation_func', function(){
+        jQuery('form#views-exposed-form-organisation-find-partner-relationship input#edit-reset').prop('type', 'button');
+        jQuery('form#views-exposed-form-organisation-find-partner-relationship input#edit-reset').on('click', function(){
           // Reset all value
-          jQuery(form_id + " option:selected").prop("selected", false);
-					jQuery(form_id + " input#edit-combine").prop('value', '');
-					jQuery(form_id + " input#edit-field-depar-name-tid").prop('value', '');	
+          jQuery("form#views-exposed-form-organisation-find-partner-relationship option:selected").prop("selected", false);
+					jQuery("form#views-exposed-form-organisation-find-partner-relationship input#edit-combine").prop('value', '');
+					jQuery("form#views-exposed-form-organisation-find-partner-relationship input#edit-field-depar-name-tid").prop('value', '');	
 	
           // Trigger click
-          jQuery(form_id + " input#edit-submit-organisation").trigger('click');
+          jQuery("form#views-exposed-form-organisation-find-partner-relationship input#edit-submit-organisation").trigger('click');
         });
       });
 	
 			// Export csv button on find partner relationship page
 			var form_id = 'form#views-exposed-form-organisation-find-partner-relationship';
-			jQuery(form_id + ' #edit-export-csv').once('export_find_partner_relation_func', function(){
-				jQuery(form_id + ' #edit-export-csv').on('click', function(){
-					var partner_search = jQuery(form_id + ' #edit-combine').prop('value');
-					var sector_search = jQuery(form_id + ' #edit-field-par-sector-tid').val();
-					var career_search = jQuery(form_id + ' #edit-field-par-career-destination-tid').val();
-					var deparment_search = jQuery(form_id + ' #edit-field-depar-name-tid').prop('value');
-					var type_search = jQuery(form_id + ' #edit-field-par-type-tid').val();
+			jQuery('form#views-exposed-form-organisation-find-partner-relationship #edit-export-csv').once('export_find_partner_relation_func', function(){
+				jQuery('form#views-exposed-form-organisation-find-partner-relationship #edit-export-csv').on('click', function(){
+					var partner_search = jQuery('form#views-exposed-form-organisation-find-partner-relationship #edit-combine').prop('value');
+					var sector_search = jQuery('form#views-exposed-form-organisation-find-partner-relationship #edit-field-par-sector-tid').val();
+					var career_search = jQuery('form#views-exposed-form-organisation-find-partner-relationship #edit-field-par-career-destination-tid').val();
+					var deparment_search = jQuery('form#views-exposed-form-organisation-find-partner-relationship #edit-field-depar-name-tid').prop('value');
+					var type_search = jQuery('form#views-exposed-form-organisation-find-partner-relationship #edit-field-par-type-tid').val();
 
 					var url = '/find-partner-relationship-csv?' + 
 						'combine=' + partner_search + '&' + 
