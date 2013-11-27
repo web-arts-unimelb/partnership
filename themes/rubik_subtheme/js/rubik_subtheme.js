7e260fe3fbb8766985bcfaf9be8350bc8430ac17
arts-unimelb <arts-webmaster@unimelb.edu.au>
2013-11-27 14:15:14 +1100

    Able to hide show programs based on type

--- themes/rubik_subtheme/js/rubik_subtheme.js
+++ themes/rubik_subtheme/js/rubik_subtheme.js
@@ -1,6 +1,35 @@
 (function ($) {
   Drupal.behaviors.rubik_subtheme = {
     attach:function(context, settings) {
+			_type_to_hide_show_program();
 		}
 	}
+
+	function _type_to_hide_show_program() {
+		// Because the option is dynamically added. 'Ready' is not going to work, so use waitUntilExists plugin
+		// https://gist.github.com/buu700/4200601
+		$('form#partnership-node-form #edit-field-par-type select.shs-select-level-1 option').waitUntilExists(function() {
+			// Initial
+			var init_selected_text = jQuery('form#partnership-node-form #edit-field-par-type select.shs-select-level-1').find('option:selected').text(); 
+			if(init_selected_text == 'Teaching and Learning' || init_selected_text == 'Engagement') {
+      	jQuery('form#partnership-node-form #edit-field-par-program').show();
+      }
+      else {
+        jQuery('form#partnership-node-form #edit-field-par-program').hide();
+      }
+
+			// Change
+			// 
+      jQuery('form#partnership-node-form #edit-field-par-type select.shs-select-level-1').change(function(){
+				var selected_text = jQuery(this).find('option:selected').text();
+				if(selected_text == 'Teaching and Learning' || selected_text == 'Engagement') {
+					jQuery('form#partnership-node-form #edit-field-par-program').show();
+				}
+				else {
+					jQuery('form#partnership-node-form #edit-field-par-program').hide();
+				} 
+      });
+    });
+  }
+
 }(jQuery));
